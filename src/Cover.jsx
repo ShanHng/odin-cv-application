269a7f4ed7d93356e7b7f0d655cda9925ee17e59
ShanHng <hngshanhern@gmail.com>
2024-09-13 03:33:01 +0800

    Align page content

--- src/Cover.jsx
+++ src/Cover.jsx
@@ -3,13 +3,15 @@ import './Cover.css'
 function Cover ({ name, aboutMe, contactNo, email, imgUrl }) {
   return (
     <div className='cover-outer-container'>
-      <div className='cover-info-container'>
-        <h1 className='cover-name'> {name} </h1>
-        <div className='cover-about-me'> {aboutMe} </div>
-        <div className='cover-contact-no'>{contactNo}</div>
-        <div className='cover-email'>{email}</div>
+      <div className='width-70vw' style={ { display: 'flex', justifyContent: 'space-between', gap: '2rem'}}>
+        <div className='cover-info-container'>
+          <h1 className='cover-name'> {name} </h1>
+          <div className='cover-about-me'> {aboutMe} </div>
+          <div className='cover-contact-no'>{contactNo}</div>
+          <div className='cover-email'>{email}</div>
+        </div>
+        <img className='cover-profile-pic' src={imgUrl} height='400px'></img>
       </div>
-      <img className='cover-profile-pic' src={imgUrl} height='400px'></img>
     </div>
   )
 }
--- src/Section.jsx
+++ src/Section.jsx
@@ -3,18 +3,20 @@ import './Section.css'
 
 function Section ({ title, sectionItems }) {
   return (
-    <div className='section-container'>
-      <h2 className='section-title'>{title}</h2>
-      <ul className='section-item-list'>
-        {sectionItems.map((item, index) => (
-          <SectionItem
-            title={item.title}
-            duration={item.duration}
-            itemDetails={item.details}
-            key={index}
-          />
-        ))}
-      </ul>
+    <div className='section-container' style={ { display: ' flex' , justifyContent: 'center'  } }>
+      <div className='width-70vw'>
+        <h2 className='section-title'>{title}</h2>
+        <ul className='section-item-list'>
+          {sectionItems.map((item, index) => (
+            <SectionItem
+              title={item.title}
+              duration={item.duration}
+              itemDetails={item.details}
+              key={index}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
